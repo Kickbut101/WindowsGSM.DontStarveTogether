c6835fd1f9b36ca363ba68c55893e13604500966
Kickbut101 <bansheedriver400@hotmail.com>
2021-02-21 14:37:49 -0600

    First commit

--- DST.cs/DST.cs
+++ DST.cs/DST.cs
@@ -20,7 +20,7 @@ namespace WindowsGSM.Plugins
             name = "WindowsGSM.DontStarveTogether", // WindowsGSM.XXXX
             author = "Andy",
             description = "🧩 WindowsGSM plugin for supporting Don't Starve Together Dedicated Server",
-            version = "1.0",
+            version = "1.1",
             url = "https://github.com/Kickbut101/WindowsGSM.DontStarveTogether", // Github repository link (Best practice)
             color = "#800080"
         };
@@ -37,50 +37,62 @@ namespace WindowsGSM.Plugins
 
 
         // - Game server Fixed variables
-        public string directoryPath = @"\bin\";
-        public override string StartPath => @"dontstarve_dedicated_server_nullrenderer.exe"; // Game server start path, for DST, it is \bin\dontstarve_dedicated_server_nullrenderer.exe
+        public override string StartPath => @"bin/dontstarve_dedicated_server_nullrenderer.exe"; // Game server start path, for DST, it is \bin\dontstarve_dedicated_server_nullrenderer.exe
         public string FullName = "Don't Starve Together"; // Game server FullName
-        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
-        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+        public bool AllowsEmbedConsole = true;
+        public int PortIncrements = 3; // This tells WindowsGSM how many ports should skip after installation
         public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
 
 
         // - Game server default values
-        public string Port = "10999"; // Default port
-        public string QueryPort = "27016"; // Default query port
-        public string Defaultmap = "MyDediServer"; // Default map name
-        public string Maxplayers = "20"; // Default maxplayers
+        public string Port = "10999"; // Default port (called "server_port") in server.ini
+        public string QueryPort = "27016"; // Default query port (Called "master_server_port") in server.ini
+        public string Defaultmap = "MyDediServer"; // Default world and shard name
+        public string Maxplayers = "10"; // Default maxplayers
         public string Additional = ""; // Additional server start parameter
 
 
         // - Create a default cfg for the game server after installation
         public async void CreateServerCFG() 
-        { 
-            var sb = new StringBuilder();
-            sb.AppendLine($"motd={_serverData.ServerName}");
-        }
+        {}
 
 
         // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-            string shipWorkingPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID);
-            shipWorkingPath = shipWorkingPath + directoryPath;
-            string shipExePath = shipWorkingPath + StartPath;
+            string shipWorkingPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID); // c:\windowsgsm\servers\1\serverfiles\
+            string shipWorkingBinPath = Path.Combine(ServerPath.GetServersServerFiles(_serverData.ServerID, @"bin/")).ToString(); // c:\windowsgsm\servers\1\serverfiles\bin
+            string shipWorkingEXEPathFull = Path.Combine(ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath)); // c:\windowsgsm\servers\1\serverfiles\ + bin\dontstarve_dedicated_server_nullrenderer.exe
 
-            if (!File.Exists(shipExePath))
+            // Flip the backslashes for forwards slashes. Unsure if this was necessary.
+            shipWorkingBinPath = shipWorkingBinPath.Replace(@"\","/");
+
+            // Does \bin\ path exist?
+            if(!Directory.Exists(shipWorkingBinPath))
             {
-                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                Error = $"Directory not found - ({shipWorkingBinPath})";
                 return null;
-            }	
+            }
 
-            // Prepare start parameter THESE NEED TO BE MODIFIED!!!!!!!!
-            // MODIFY THESESESETESESEE
+            // Does .exe path exist?
+            if (!File.Exists(shipWorkingEXEPathFull))
+            {
+                Error = $"{Path.GetFileName(shipWorkingEXEPathFull)} not found in ({shipWorkingPath})";
+                return null;
+            }
+
+            // Prepare start parameters
             var param = new StringBuilder();
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -steam_master_server_port {_serverData.ServerPort}");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $" -name=\"{_serverData.ServerName}\"");
-            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}"); 
- 
+            param.Append($" -persistent_storage_root \"{shipWorkingBinPath}\"");
+            param.Append($" -conf_dir serverdatafolder");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerMap) ? string.Empty : $" -cluster \"{_serverData.ServerMap}\""); 
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerMap) ? string.Empty : $" -shard \"{_serverData.ServerMap}_Shard\"");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -port {_serverData.ServerPort}");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -steam_master_server_port {_serverData.ServerQueryPort}");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer) ? string.Empty : $" -players {_serverData.ServerMaxPlayer}");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}");
+
+
             // Prepare Process
             var p = new Process
             {
@@ -88,9 +100,9 @@ namespace WindowsGSM.Plugins
                 {
                     WindowStyle = ProcessWindowStyle.Minimized,
                     UseShellExecute = false,
-                    WorkingDirectory = shipWorkingPath,
-                    FileName = shipExePath,
-                    Arguments = ""
+                    WorkingDirectory = shipWorkingBinPath,
+                    FileName = shipWorkingEXEPathFull,
+                    Arguments = param.ToString()
                 },
                 EnableRaisingEvents = true
             };
